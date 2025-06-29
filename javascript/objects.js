841d858911dc865627509474c9c110622a4058b7
Soiidux <soiidux040@gmail.com>
2025-06-30 02:37:25 +0530

    fixed nested object

--- javascript/objects.js
+++ javascript/objects.js
@@ -1,31 +1,47 @@
 //Problem: Create an object representing a type of tea with properties for name type and caffeine content.
-const teas={
+const tea={
     tname: "Lemon tea",
     type:"Green",
     caffeine:"low"
 }
 
 //Access and print the name and type properties of the tea object
-console.log(teas.tname);
-console.log(teas["type"]);
-console.log(teas.caffeine);
+console.log(tea.tname);
+console.log(tea["type"]);
+console.log(tea.caffeine);
 
 //Add a new property origin to the tea object
-teas.origin = "Assam";
+tea.origin = "Assam";
 
 //Change the caffeine level of the tea object to medium
-teas.caffeine="Medium";
+tea.caffeine="Medium";
 
 //Problem:Remove the propery type from the object
-delete teas.type;
+delete tea.type;
 
 //Check if the tea object has a property origin
-console.log("origin" in teas);
-console.log(Object.hasOwn(teas,"origin"));
-console.log(teas.origin!==undefined);
+console.log("origin" in tea);
+console.log(Object.hasOwn(tea,"origin"));
+console.log(tea.origin!==undefined);
 
 
 //Use a for loop to print all properties of the tea object
-for (key in teas){
-    console.log(key+": "+teas[key]);
+for (key in tea){
+    console.log(key+": "+tea[key]);
 }
+
+//Create a nested object representing different types of tea and their properties
+const teas={
+    greentea:{
+        tname:"Green Tea",
+        caffeine=:"low"
+    },
+    blacktea:{
+        tname:"Black Tea",
+        caffeine:"High"
+    }
+}
+
+//Create a copy of tea object
+//Add a custom method describe to the tea object that returns a discription string.
+//Merge two objects representing different teas into one.
