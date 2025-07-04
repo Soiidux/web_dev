26b1ebef21d17956a045cb1d5b65d647f2460d7d
Soiidux <soiidux040@gmail.com>
2025-07-05 01:02:16 +0530

    Building: Building a basic todo app with html and js

---
+++ todo_app/index.js
@@ -0,0 +1,13 @@
+const addBtn = document.getElementById("add-btn");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+addBtn.addEventListener("click",function(){
+    const value = todoInput.value;
+    const li=document.createElement("li");
+    li.innerText=value;
+    todoList.appendChild(li);
+    todoInput.value="";
+    const xbutton=document.createElement("button");
+    xbutton.innerText="X";
+    li.appendChild(xbutton);
+})
