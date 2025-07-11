df8cef1882c6426870d86d917de2f0f65b1bfffa
Soiidux <soiidux040@gmail.com>
2025-07-11 20:56:14 +0530

    Learning: Proxies

---
+++ javascript/proxies.js
@@ -0,0 +1,45 @@
+//Proxy such that user is not able to change the data
+
+let person = {
+    fname: "Yash",
+    lname: "Nagpal",
+    age: 18
+}
+
+let personProxy = new Proxy(person, {
+    get(target, prop) {
+        if (prop in target){
+            return target[prop];
+        }
+        else{
+            return "Invalid propery"
+        }
+    },
+    set(target,prop,value){
+        if(!(prop in target)){
+            throw new Error(`${prop} does not exist`);
+        }
+        switch(prop){
+            case 'fname':
+            case 'lname':
+                if(typeof value !== 'string' ){
+                    throw new Error(`${prop} must be a string`);
+                }
+            break;
+            case 'age':
+                if(typeof value!== 'number') throw new Error(`${prop} must be a number`)
+                if(value<=0) throw new Error(`${prop} must be greater than 0`);
+            break;
+            
+            
+        }
+        target[prop]=value;
+        }
+})
+
+console.log(personProxy.age);
+// console.log(personProxy.password);
+// personProxy.age=-10;
+// personProxy.age="abc";
+// personProxy.fname=1;
+
