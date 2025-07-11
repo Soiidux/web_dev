37d78a1b476e3012ce3f3e29d68d98382287a556
Soiidux <soiidux040@gmail.com>
2025-07-12 03:49:18 +0530

    Building: Wrote a function that updates the clock and runs are regular intervals

---
+++ clock/script.js
@@ -0,0 +1,12 @@
+function updateClock(){
+    let timeElement = document.getElementById("time");
+    let dateElement = document.getElementById("date");
+    const now = new Date();
+    const hours = (now.getHours()%12 || 12).toString().padStart(2,"0");
+    const minutes = now.getMinutes().toString().padStart(2,"0");
+    const seconds = now.getSeconds().toString().padStart(2,"0");
+    const AMPM = now.getHours() >= 12? "PM" : "AM";
+    timeElement.textContent = `${hours}:${minutes}:${seconds} ${AMPM}`;
+}
+updateClock();
+setInterval(updateClock,1000);
