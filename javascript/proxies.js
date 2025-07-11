759ceafa3b1ba886e9b807d0ac39ecb4b14484a7
Soiidux <soiidux040@gmail.com>
2025-07-12 02:10:16 +0530

    Learning: Implementing negative indexing in arrays using proxy

--- javascript/proxies.js
+++ javascript/proxies.js
@@ -77,3 +77,35 @@ let person2Proxy = new Proxy(person2,{
 // console.log(person2Proxy.age);     
 // console.log(person2Proxy.password); 
 // person2Proxy.age = 25;    
+
+
+//basic implementation for negative indexing in an array
+arr=[1,2,3,4,5];
+
+let arrProxy = new Proxy(arr,{
+    get(target,prop){
+        if(prop<0){
+            return target[target.length+ Number(prop)];
+
+        }
+        else{
+            return Reflect.get(target,prop);
+        }
+    },
+    set(target,prop,value){
+        if(Number(prop)<0){
+            newprop = target.length + Number(prop);
+            return Reflect.set(target,newprop,value);
+        }
+        else{
+            return Reflect.set(target,prop,value);
+        }
+    }
+})
+
+console.log(arrProxy[-1]);
+// arrProxy[-1]=4;
+arrProxy[-3]=-1;
+arrProxy.splice(-2,1);
+console.log(arrProxy);
+
