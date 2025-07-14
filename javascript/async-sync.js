62d0f3af6c58949d1e803a74ed011d54136dfd1c
Soiidux <soiidux040@gmail.com>
2025-07-15 03:08:14 +0530

    Learning:Making async code more readable using async and await

--- javascript/async-sync.js
+++ javascript/async-sync.js
@@ -8,33 +8,33 @@
 // @@@@@@   LEGACY CODE   @@@@@@
 // -----------------------------------------------------------------------------------------------------------------------------
 
-const fs = require("fs");
-console.log("Starting Program");
-fs.readFile("./hello.txt","utf-8",function(err,response){   //Reading hello.txt
-    if (err){
-        console.log("Error reading hello.txt"); //error messages
-    }
-    else{
-        console.log("Hello.txt was read successfully")
-        fs.writeFile("backup.txt",response,function(err){   //Making backup.txt and writing on it
-            if(err){
-                console.log("Error in writing backup.txt");
-            }
-            else{
-                console.log("Successfully written backup.txt");
-                fs.unlink("./hello.txt",function(err){
-                    if(err){
-                        console.log("Error in deleting hello.txt");
-                    }
-                    else{
-                        console.log("hello.txt deleted successfully");
-                    }
-                })
-            }
-        })
-    }
-})
-console.log("Program ended");
+// const fs = require("fs");
+// console.log("Starting Program");
+// fs.readFile("./hello.txt","utf-8",function(err,response){   //Reading hello.txt
+//     if (err){
+//         console.log("Error reading hello.txt"); //error messages
+//     }
+//     else{
+//         console.log("Hello.txt was read successfully")
+//         fs.writeFile("backup.txt",response,function(err){   //Making backup.txt and writing on it
+//             if(err){
+//                 console.log("Error in writing backup.txt");
+//             }
+//             else{
+//                 console.log("Successfully written backup.txt");
+//                 fs.unlink("./hello.txt",function(err){
+//                     if(err){
+//                         console.log("Error in deleting hello.txt");
+//                     }
+//                     else{
+//                         console.log("hello.txt deleted successfully");
+//                     }
+//                 })
+//             }
+//         })
+//     }
+// })
+// console.log("Program ended");
 // -----------------------------------------------------------------------------------------------------------------------------
 
 
@@ -43,51 +43,51 @@ console.log("Program ended");
 // @@@@@@   CONVERTING LEGACY CODE TO MODERN CODE   @@@@@@
 // -----------------------------------------------------------------------------------------------------------------------------
 
-// const fs = require('fs');
-// function ReadFileWithPromise(filepath,encoding){
-//     return new Promise((resolve,reject)=>
-//     {
-//         fs.readFile(filepath,encoding,(error,content)=>
-//         {
-//             if(error){
-//                 reject(error);                                  //Signal to user's .catch function to run
-//             }
-//             else{
-//                 resolve(content);                               //Signal to user's .then function to run
-//             }       
-//         })
-//     })
-// }
+const fs = require('fs');
+function ReadFileWithPromise(filepath,encoding){
+    return new Promise((resolve,reject)=>
+    {
+        fs.readFile(filepath,encoding,(error,content)=>
+        {
+            if(error){
+                reject(error);                                  //Signal to user's .catch function to run
+            }
+            else{
+                resolve(content);                               //Signal to user's .then function to run
+            }       
+        })
+    })
+}
 
-// function WriteFileWithPromise(filename,content){
-//     return new Promise((resolve,reject)=>
-//     {
-//         fs.writeFile(filename,content,function(error)
-//         {
-//            if(error){
-//             reject(error);
-//            } 
-//            else{
-//             resolve(`${filename} was written successfully`);
-//            }
-//         })
-//     })
-// }
+function WriteFileWithPromise(filename,content){
+    return new Promise((resolve,reject)=>
+    {
+        fs.writeFile(filename,content,function(error)
+        {
+           if(error){
+            reject(error);
+           } 
+           else{
+            resolve(`${filename} was written successfully`);
+           }
+        })
+    })
+}
 
-// function DeleteFileWithPromise(filepath){
-//     return new Promise((resolve,reject)=>
-//         {
-//             fs.unlink(filepath,function(error)
-//             {
-//                 if(error){
-//                     reject(error);
-//                 }
-//                 else{
-//                     resolve("File was deleted.");
-//                 }
-//             })
-//         })
-// }
+function DeleteFileWithPromise(filepath){
+    return new Promise((resolve,reject)=>
+        {
+            fs.unlink(filepath,function(error)
+            {
+                if(error){
+                    reject(error);
+                }
+                else{
+                    resolve("File was deleted.");
+                }
+            })
+        })
+}
 // console.log("Program start");
 // ReadFileWithPromise('./hello.txt','utf-8')
 //     .then((content)=>WriteFileWithPromise('backup.txt',content))
@@ -97,8 +97,29 @@ console.log("Program ended");
 //     })
 //     .then((Deletemessage)=>console.log(Deletemessage))
 //     .catch((error)=>console.log(`Error: ${error}`))
+//     .finally(()=>console.log("All done"))
 // console.log("Program end.")
 // -----------------------------------------------------------------------------------------------------------------------------
+
+//  @@@@@@ CONVERTING THE ABOVE CODE INTO A MORE READABLE CODE USING ASYNC AND AWAIT :Async code looks like sync code   @@@@@@
+// -----------------------------------------------------------------------------------------------------------------------------
+async function DoTasks() {
+    try{
+        const filecontent = await ReadFileWithPromise('./hello.txt','utf-8')
+        await WriteFileWithPromise('backup.txt',filecontent)
+        await DeleteFileWithPromise('./hello.txt') 
+    }
+    catch{
+        (error)=>console.log(`Error: ${error}`)
+    }
+    finally{
+        console.log("All done");
+    }
+}
+
+DoTasks().then(()=> console.log("All done"));
+
+
 
 
 
