491e5fd80a32bcfd9e8bf32243aa7be3c12c7480
Soiidux <soiidux040@gmail.com>
2025-07-18 02:05:26 +0530

    Learning:In-depth about Function.prototype.apply()

--- javascript/call-bind-apply.js
+++ javascript/call-bind-apply.js
@@ -5,8 +5,9 @@ If the given function (say , func1) calls another function (func2) which is defi
 on top of the main execution context. As a result if a call in made to func2 it will run however not in way we want it to, it will be executed 
 and removed out of the call stack without inheriting func1's 'this' . Instead func2's this defaults to window(browser) / global(node.js)
 in non-strict mode and undefined in strict mode.
+
 This has been a big problem in JS. A way to fix this is using .call(). Call explicitly sets this for a function (thisArgument). 
-It calls the given function with a given 'this' values and arguments provided individually (arg1,arg2,arg3)
+It calls the given function with a given 'this' value and arguments provided individually (arg1,arg2,arg3)
 */
 
 //###############################################################################################################################
@@ -31,4 +32,20 @@ console.log(user);
 
 
 
+//  APPLY
+/* USAGE: function.apply(thisArgument,[arg1,arg2,arg3,...])
+The apply() method of a function is similar to the call() method, it alos explicitly sets 'this' for a function with a given value 
+(thisArgument), the only difference is that the arguments to the function are passed as an array([arg1,arg2,arg3,...]) or an array-like-object.
+*/
 
+const printDetails = function(Department, Floor){
+    console.log(this.name + " | " + Department + " | " + Floor);
+}
+
+const UserDetails = {
+    name: "Yash",
+    age: 18,
+    gender: "Male"
+};
+
+printDetails.apply(UserDetails,["App testing","First Floor"]);
