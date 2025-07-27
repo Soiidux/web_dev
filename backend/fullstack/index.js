fa92bc0a05acab05fdc62661967086eff6a3f2d2
Soiidux <soiidux040@gmail.com>
2025-07-28 03:19:52 +0530

    Installed mongodb and improved code

--- backend/fullstack/index.js
+++ backend/fullstack/index.js
@@ -1,7 +1,8 @@
 // const express = require('express')   //only valid when type is classicjs in package.json
 import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
+import dotenv from "dotenv" //Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env.
+import cors from "cors" //one of the biggest headaches in dev world
+import dbConnect from "./utils/db.js"; //Importing a function to connect to a db
 
 // dotenv.config(pathname)
 dotenv.config()
@@ -12,9 +13,14 @@ const port = process.env.PORT || 3000
 
 app.use(cors({
   //configuration
-  origin : "https://localhost:3000",
-  methods : ["GET","POST","DELETE","OPTIONS"]
+  origin : process.env.BASE_URL,                  //this or an array of origins, origin is from where I want the request to come
+  methods : ["GET","POST","DELETE","OPTIONS"],        //not case sensitive
+  allowedHeaders : ["Content-Type","Authorization"]   //case sensitive
 }))
+
+
+app.use(express.json())                               //now the server accepts json
+app.use(express.urlencoded({extended:true}))          //lets your app read form data sent from websites.
 
 //we are making a web server
 
@@ -30,6 +36,8 @@ app.get("/lastname",(request,response) => {
     response.send("Nagpal")
 })
 
+//connect to db
+dbConnect();
 console.log(process.env.PORT);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
