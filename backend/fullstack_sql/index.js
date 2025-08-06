07e1a77c91e655cd2a0c132648d8aa13cb429f1f
Soiidux <soiidux040@gmail.com>
2025-08-07 01:35:23 +0530

    Learning: Writing boilerplate code for index.js and making controller/ and routes/

---
+++ backend/fullstack_sql/index.js
@@ -0,0 +1,28 @@
+import express from "express.js"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+const app = express();                                      //gives all the powers of express to a variable called app
+dotenv.config();                                            //process.env is setup
+const port = process.env.PORT;                             
+
+app.use(cookieParser());                                      //cookieParser is used to access cookies
+app.use(express.json());                                    //Allows json to be taken as input
+app.use(express.urlencoded({extended:true}));               //Allows data to be taken from url
+
+
+app.use(cors({                                              //Using cors
+    origin: process.env.BASE_URL
+}))
+
+app.listen(port,()=>{                                       //Express is now listening at port
+    console.log(`Backend is listening at port ${port}`);
+});
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        success:true,
+        message:"Reached /"
+    })
+})
