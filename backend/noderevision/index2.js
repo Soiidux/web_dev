0dd2552293d26f803bf9372393166a7cf6c202da
Soiidux <soiidux040@gmail.com>
2025-08-22 03:37:42 +0530

    Learning: Middleware and Handler functions

--- backend/noderevision/index2.js
+++ backend/noderevision/index2.js
@@ -16,27 +16,62 @@ const users = [
         name:"Jim Doe"
     }
 ]
-const server = createServer((req,res)=>{
-    try {
-        res.setHeader('Content-Type','text/html');
-        if(req.url === '/api/users' && req.method === 'GET'){
-            res.write(JSON.stringify(users));
-        }
-        else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
-            const id = req.url.split('/')[3];
-            const user = users.find((user)=>user.id === parseInt(id));
-            res.write(JSON.stringify(user));
-        }
-        else{
-            res.statusCode = 404;
-            res.write("<h1>404 : Not found<\h1>");
-        }
-    } catch (error) {
-        console.log(`Error: ${error}`);
-        res.statusCode = 500;
-        res.write("<h1>Server error<\h1>")
-    }
+
+//Logger middleware
+const logger = (req,res,next) => {
+    console.log(`Method: ${req.method}`);
+    console.log(`Route: ${req.url}`);
+    next();
+}
+
+//JSON Middleware
+const jsonMiddleware = (req,res,next)=>{
+    res.setHeader('content-type','application/json');
+    next();
+}
+
+//Route handler for GET /api/users
+const getUsersHandler = (req,res) =>{
+    res.write(JSON.stringify(users));
     res.end();
+};
+
+//Route handler for GET /api/users/id
+const getUserHandler = (req,res)=>{
+    const id = req.url.split('/')[3];
+    const user = users.find((user)=>user.id === parseInt(id));
+    res.write(JSON.stringify(user));
+    res.end();
+}
+
+//Route handler for 404
+const notfound = (req,res)=>{
+    res.statusCode = 404;
+    res.write("<h1>404 : Not found<\h1>");
+    res.end();
+}
+const server = createServer((req,res)=>{
+    logger(req,res,()=>{
+        try {
+           jsonMiddleware(req,res,()=>{
+                if(req.url === '/api/users' && req.method === 'GET'){
+                    getUsersHandler(req,res);
+                }
+                else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
+                    getUserHandler(req,res);
+                }
+                else{
+                    notfound(req,res);
+                }
+            })
+        }
+       catch (error) {
+            console.log(`Error: ${error}`);
+            res.statusCode = 500;
+            res.write("<h1>Server error<\h1>")
+            res.end();
+    }
+    })
 })
 
 server.listen(process.env.PORT,()=>{
