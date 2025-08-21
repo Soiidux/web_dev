8261d57bba7c03e1c08f7bb92fcd801daa82cffb
Soiidux <soiidux040@gmail.com>
2025-08-22 00:33:03 +0530

    Learning: Basic error handling for routes

--- backend/noderevision/index.js
+++ backend/noderevision/index.js
@@ -8,19 +8,32 @@ dotenv.config();
 const server = http.createServer((req,res)=>{
     console.log(`Route: ${req.url}`);
     console.log(`Method: ${req.method}`);
-    if(req.url === '/'){
-        res.writeHead(200,{'Content-Type':'text'});
-        res.end("<h1>Home Page</h1>");
-    }
+    try {
+        if(req.method === 'GET'){
+            if(req.url === '/'){
+                res.writeHead(200,{'Content-Type':'text'});
+                res.end("<h1>Home Page</h1>");
+            }
 
-    else if(req.url === '/about'){
-        res.writeHead(200,{'content-type':'text'});
-        res.end("<h1>About page</h1>");
+            else if(req.url === '/about'){
+                res.writeHead(200,{'content-type':'text'});
+                res.end("<h1>About page</h1>");
+            }
+            else{
+                res.writeHead(404,{"content-type":'text'});
+                res.end("<h1>404: Page not Found</h1>");
+            }
+
+        }
+        else{
+            throw new Error("Method not allowed");
+        }  
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        res.writeHead(500,{'content-type':'text'});
+        res.end("Server error")
     }
-    else{
-        res.writeHead(404,{"content-type":'text'});
-        res.end("<h1>404: Page not Found</h1>");
-    }
+    
 })
 
 //making it listen at a port
